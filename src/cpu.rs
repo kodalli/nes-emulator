c12a5fa86021044ff9e6ff5ba0d8effb819814fe
kodalli <surya.kodali@gmail.com>
2021-10-17 19:21:53 -0400

    minor fixes

--- src/cpu.rs
+++ src/cpu.rs
@@ -893,6 +893,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
+        // LDA immediate, ORA zero page, BRK
         cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 5);
         assert_eq!(cpu.status.bits() & 0b0000_0010, 0b00);
@@ -903,6 +904,7 @@ mod test {
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
         cpu.register_a = 10;
+        // TAX, BRK
         cpu.load_and_run(vec![0xaa, 0x00]);
 
         assert_eq!(cpu.register_x, 10)
@@ -911,6 +913,7 @@ mod test {
     #[test]
     fn test_5_ops_working_together() {
         let mut cpu = CPU::new();
+        // LDA immediate, CPY immediate, TAX, INX, BRK
         cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
 
         assert_eq!(cpu.register_x, 0xc1)
@@ -920,6 +923,7 @@ mod test {
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
         cpu.register_x = 0xff;
+        // INX, INX, BRK
         cpu.load_and_run(vec![0xe8, 0xe8, 0x00]);
 
         assert_eq!(cpu.register_x, 1)
@@ -929,7 +933,7 @@ mod test {
     fn test_lda_from_memory() {
         let mut cpu = CPU::new();
         cpu.mem_write(0x10, 0x55);
-
+        // LDA zero page, BPL, BRK
         cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
 
         assert_eq!(cpu.register_a, 0x55);
